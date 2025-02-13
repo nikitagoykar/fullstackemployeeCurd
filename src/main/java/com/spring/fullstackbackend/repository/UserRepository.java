04a7310b27a5293dc937addb697b6c3533865373
nikitagoykar <nikitagoykar3775@gmail.com>
2025-02-13 14:00:47 +0530

    empoloyee full stack crud

---
+++ src/main/java/com/spring/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.spring.fullstackbackend.repository;
+
+import com.spring.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
---
+++ src/test/java/com/spring/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
